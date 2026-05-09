9f82866c893577b27572d44304bc3dac9927f7c1
Barsha <S394996@students.cdu.edu.au>
2026-05-09 16:22:24 +0930

    Initializing logic folder

---
+++ logic/logic_manager.py
@@ -0,0 +1,10 @@
+#Logic Management
+class GameLogic:
+    def __init__(self):
+        self.found_differences = 0
+        self.mistakes = 0
+        self.target_coordinates = []
+
+    def verify_click(self, x, y):
+        # This will be implemented to check user clicks
+        pass
